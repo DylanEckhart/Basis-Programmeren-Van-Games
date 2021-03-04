4b01c3b3933399ed59f7b2042665fe99b1b2e1b9
Dylan Eckhart <deckhart@roc-dev.com>
2021-03-04 13:48:47 +0100

    Version 1.4

--- Phaser Introductie/js/sceneMain.js
+++ Phaser Introductie/js/sceneMain.js
@@ -6,7 +6,8 @@ class SceneMain extends Phaser.Scene {
 
     preload() {
         this.alphaFactor = -0.01;
-        this.stepLenght = 5;
+        this.stepLenght = 25;
+        this.score = 0;
         this.bananaX = game.config.width / 2;
         this.bananaY = game.config.height / 2;
         this.load.image("banana", "images/banana.png");
@@ -16,11 +17,33 @@ class SceneMain extends Phaser.Scene {
             fontSize: 50,
             color: "white"
         });
-        this.randomText.setOrigin(0.5, 0.5)
+        this.randomText.setOrigin(0.5, 0.5);
+        this.renderScore();
+    }
+
+    renderScore() {
+        this.scoreText = this.add.text(50, 50, "Score: " + this.score, {
+            fontFamily: "sans-serif",
+            fontSize: 50,
+            color: "white"
+        });
+    }
+
+    spawnBanana() {
+        this.banana = this.add.image(this.bananaX, this.bananaY, "banana");
+
+        this.banana.setInteractive();
+        this.banana.on("pointerdown", () => {
+            this.score++;
+            this.banana.destroy();
+            this.scoreText.destroy();
+            this.renderScore();
+            this.spawnBanana();
+        });
     }
 
     create() {
-        this.banana = this.add.image(this.bananaX, this.bananaY, "banana");
+        this.spawnBanana();
     }
 
     update() {
@@ -28,12 +51,12 @@ class SceneMain extends Phaser.Scene {
         if (this.banana.alpha === 0) {
             this.alphaFactor = 0.01;
         }
-        if (this.banana.alpha === 0) {
+        if (this.banana.alpha === 1) {
             this.alphaFactor = -0.01;
         }
         this.banana.alpha += this.alphaFactor;
 
-        // Moving behaviour
+        // Horizontal behaviour
         if (this.banana.x >= game.config.width) {
             this.stepLenght = this.stepLenght * -1;
             this.banana.flipX = true;
@@ -43,6 +66,15 @@ class SceneMain extends Phaser.Scene {
             this.banana.flipX = false;
         }
         this.banana.x += this.stepLenght;
+
+        // Vertical behaviour
+        if (this.banana.y >= game.config.height) {
+            this.stepLenght = this.stepLenght * -1;
+        }
+        if (this.banana.y <= 0) {
+            this.stepLenght = this.stepLenght * -1;
+        }
+        this.banana.y += this.stepLenght;
     }
 
 }
