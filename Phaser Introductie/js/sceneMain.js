2f75044cb0166b84ab9ebeece7b02274cdd69d0b
Dylan Eckhart <deckhart@roc-dev.com>
2021-03-04 10:20:57 +0100

    Version 1.2

--- Phaser Introductie/js/sceneMain.js
+++ Phaser Introductie/js/sceneMain.js
@@ -6,10 +6,28 @@ class SceneMain extends Phaser.Scene {
 
     preload() {
         // This is where we load things into memory
+        this.load.image("mario", "images/mario.png");
+        this.load.image("hero", "images/hero.png", {
+            frameWidth: 32,
+            frameHeight: 32
+        });
     }
 
     create() {
         // This is where we create an manipulate objects
+        this.mario = this.add.image(50, 50, "mario");
+        this.hero = this.add.sprite(240, 320, "hero");
+        this.anims.create({
+            key: "hero_walk",
+            frames: [
+                { key: "hero", frame: 1 },
+                { key: "hero", frame: 2 },
+                { key: "hero", frame: 3 },
+                { key: "hero", frame: 4 }
+            ],
+            frameRate: 4,
+            repeat: -1
+        });
     }
 
     update() {
