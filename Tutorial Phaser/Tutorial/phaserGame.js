6f788b5e79675500f2a422af427e440ffd6c067e
Dylan Eckhart <deckhart@roc-dev.com>
2021-03-11 10:51:50 +0100

    Version 2.5

---
+++ Tutorial Phaser/Tutorial/phaserGame.js
@@ -0,0 +1,32 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+var stars;
+var bombs;
+var platforms;
+var cursors;
+var score = 0;
+var gameOver = false;
+var scoreText;
+
+var game = new Phaser.Game(config);
+
+function preload () {
+    this.load.image('sky')
+}
