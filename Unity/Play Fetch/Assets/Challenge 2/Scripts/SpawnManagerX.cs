948914f1b8b8f81d11f389b6388babaa19e9f1a3
Dylan Eckhart <deckhart@roc-dev.com>
2021-03-18 12:06:42 +0100

    Version 1.3

--- Unity/Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Unity/Play Fetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnManagerX : MonoBehaviour
 {
@@ -13,20 +15,25 @@ public class SpawnManagerX : MonoBehaviour
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
 
+    System.Random random = new System.Random();
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, random.Next(3, 5));
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        
+        int number = random.Next(0, 2);
+        
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[number], spawnPos, ballPrefabs[number].transform.rotation);
     }
 
 }
