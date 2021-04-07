b11b65e7db7290018a35569c2e066024ce87a024
Dylan Eckhart <deckhart@roc-dev.com>
2021-04-07 12:01:54 +0200

    Version 1.0

---
+++ Unity/My own game/Assets/CarController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
