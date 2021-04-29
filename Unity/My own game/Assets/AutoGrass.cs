2828cab79b8a30d0bb23394fdacca9e86fc48c3e
Dylan Eckhart <deckhart@roc-dev.com>
2021-04-29 12:12:18 +0200

    Test

--- Unity/My own game/Assets/AutoGrass.cs
+++ Unity/My own game/Assets/AutoGrass.cs
@@ -13,7 +13,7 @@ public class AutoGrass : EditorWindow
     public float density = 1;
 
     [MenuItem("DylanEckhart/AutoGrass")]
-    //wdhwuhdwhdwygdywgdyw
+    //wgdywdgyw
     public static void ShowWindow()
     {
         EditorWindow.GetWindow(typeof(AutoGrass));
