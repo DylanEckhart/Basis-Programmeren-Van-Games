5a6c481b6beb9cd74f3013c8749ed858598cf5e0
Dylan Eckhart <deckhart@roc-dev.com>
2022-01-24 14:11:33 +0100

    Made a timer script, but having some problems

--- Examen/Assets/Scripts/TimerController.cs
+++ Examen/Assets/Scripts/TimerController.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static TimerController instance;
+
+    public Text timeCounter;
+
+    private TimeSpan timePlaying;
+    private bool timerGoing;
+    private float elapsedTime;
+
+    private void Awake()
     {
+        instance = this;
+    }
+
+    // Standard text value of the timer
+    private void Start()
+    {
+        timeCounter.text = "Time: 00:00:00";
+        timerGoing = false;
+        instance.BeginTimer();
+    }
+
+    // Start running the timer
+    public void BeginTimer()
+    {
+        timerGoing = true;
+        elapsedTime = 0f;
+        StartCoroutine(UpdateTimer());
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartCoroutine(IEnumerable enumerable)
     {
-        
+        throw new NotImplementedException();
+    }
+
+    // End the timer
+    public void EndTimer()
+    {
+        timerGoing = false;
+    }
+
+    // Update the timer per second
+    private IEnumerable UpdateTimer()
+    {
+        while (timerGoing)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            string timePlayingStr = "Time: " + timePlaying.ToString("mm' : 'ss' . 'f");
+            timeCounter.text = timePlayingStr;
+
+            yield return null;
+        }
     }
 }
